bdb780ae1ee798bc16ebc1d66bc3ac8a4e9c1bb8
takahara tomoya <t.tkorock221@gmail.com>
2024-04-21 10:07:41 +0900

    ログイン処理を追加

--- app/routes/dynamic.tsx
+++ app/routes/dynamic.tsx
@@ -1,6 +1,17 @@
-import {
-    Outlet,
-} from "@remix-run/react";
+import { Outlet, useRouteError, isRouteErrorResponse } from "@remix-run/react";
+import { getSession } from "~/session";
+import { isSession, json } from "@remix-run/node";
+
+export const loader = async () => {
+    const loginSession = await getSession();
+    console.log(loginSession)
+    const loginFlag = isSession(loginSession);
+    if(loginFlag){
+        return json({ ok: true });
+    } else {
+        throw new Response("", { status: 404 });
+    }
+};
 
 export default function Index(){
     return(
@@ -10,4 +21,15 @@ export default function Index(){
             <footer>これがフッター</footer>
         </div>
     )
+}
+
+export function ErrorBoundary() {
+    // NOTE: これはサーバーエラーの時のみ(loader側でthrowした場合のみ)
+    const error = useRouteError();
+    console.log('root boundary',error);
+    if (isRouteErrorResponse(error)) {
+        return(
+            <div>404です</div>
+        )
+    }
 }
